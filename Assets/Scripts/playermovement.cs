2f7a40121fbd85c2cdb302b2ab351217b9271b47
coltfields23 <111153179+coltfields23@users.noreply.github.com>
2022-09-07 15:13:27 -0500

    updated files

--- Assets/Scripts/playermovement.cs
+++ Assets/Scripts/playermovement.cs
@@ -8,37 +8,55 @@ public class playermovement : MonoBehaviour
     private float Move;
 
     public float jump;
+    private int extraJumps;
+    public int extraJumpsValue;
 
-    public bool isJumping;
-    
+    public bool isGrounded;
+
+    private bool facingRight = true;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Move = Input.GetAxis("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+    }
+    void Update(){
 
+        Move = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(speed * Move, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && isJumping == false)
-        {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
-            isJumping = true;
-            Debug.Log("jump");
+        if(isGrounded == true){
+            extraJumps = extraJumpsValue;
         }
+        if(Input.GetKeyDown(KeyCode.W) && extraJumps > 0){
+            rb.velocity = Vector2.up * jump;
+            extraJumps--;
+        } else if(Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jump;
+        }
+        // if(facingRight == false && Move > 0){
+        //     Flip();
+        // } else if(facingRight == true && Move < 0) {
+        //     Flip();
+        // }
     }
+    // void Flip(){
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            isJumping = false;
-        }
-    }
+    //     facingRight = !facingRight;
+    //     Vector3 Scaler = transform.localScale;
+    //     Scaler.x *= -1;
+    //     transform.localScale = Scaler;
+
+    // }
 }
