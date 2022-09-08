513a051371b173a6bf391c4509498ce46de210a0
coltfields23 <111153179+coltfields23@users.noreply.github.com>
2022-09-08 15:57:44 -0500

    updated movement + sprite

--- Assets/Scripts/playermovement.cs
+++ Assets/Scripts/playermovement.cs
@@ -1,62 +1,92 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-    public float speed;
-    private float Move;
+    private float horizontal;
+    public float speed = 8f;
+    public float jumpingPower = 16f;
+    public float jumps = 1;
+    private bool isFacingRight = true;
 
-    public float jump;
-    private int extraJumps;
-    public int extraJumpsValue;
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingPower = 24f;
+    public float dashingTime = 0.2f;
+    public float dashingCooldown = 1f;
 
-    public bool isGrounded;
 
-    private bool facingRight = true;
-    public Transform groundCheck;
-    public float checkRadius;
-    public LayerMask whatIsGround;
-    private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        extraJumps = extraJumpsValue;
-        rb = GetComponent<Rigidbody2D>();
+        if (isDashing)
+        {
+            return;
+        }
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+        Flip();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-    }
-    void Update(){
-
-        Move = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
-
-        if(isGrounded == true){
-            extraJumps = extraJumpsValue;
+        if (isDashing)
+        {
+            return;
         }
-        if(Input.GetKeyDown(KeyCode.W) && extraJumps > 0){
-            rb.velocity = Vector2.up * jump;
-            extraJumps--;
-        } else if(Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true){
-            rb.velocity = Vector2.up * jump;
-        }
-        // if(facingRight == false && Move > 0){
-        //     Flip();
-        // } else if(facingRight == true && Move < 0) {
-        //     Flip();
-        // }
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
-    // void Flip(){
 
-    //     facingRight = !facingRight;
-    //     Vector3 Scaler = transform.localScale;
-    //     Scaler.x *= -1;
-    //     transform.localScale = Scaler;
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
 
-    // }
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
 }
